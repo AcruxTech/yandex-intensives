09618b74ac5b684ce9e7a8dc3b8d21402d84e5f9
AcruxTech <acrux_tech@mail.ru>
2022-10-31 18:23:31 +0300

    create project struct

---
+++ 3-react-basics/src/constants/books.js
@@ -0,0 +1,85 @@
+export const books = [
+    {
+        title: 'Краткая история Земли. Четыре миллиарда лет в восьми главах',
+        annotation: 'Хорошо ли мы знаем землю под нашими ногами Скорее всего, то место, где сейчас стоит ваш дом, когда-то омывалось бурлящими потоками лавы, пряталось под толстым ледяным покровом, сотрясалось от удара метеорита, задыхалось от ядовитых газов, утопало в океане, возносилось на вершину горного хребта или давало приют устрашающим доисторическим животным.',
+        author: 'Нолл Эндрю',
+        category: 'Нехудожественная литература',
+        genre: 'Естественные науки',
+        cost: '625 ₽',
+        stars: '5',
+        reviews: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! '
+        ]
+    },
+
+    {
+        title: 'Краткая история Земли. Четыре миллиарда лет в восьми главах',
+        annotation: 'Хорошо ли мы знаем землю под нашими ногами Скорее всего, то место, где сейчас стоит ваш дом, когда-то омывалось бурлящими потоками лавы, пряталось под толстым ледяным покровом, сотрясалось от удара метеорита, задыхалось от ядовитых газов, утопало в океане, возносилось на вершину горного хребта или давало приют устрашающим доисторическим животным.',
+        author: 'Нолл Эндрю',
+        category: 'Нехудожественная литература',
+        genre: 'Естественные науки',
+        cost: '625 ₽',
+        stars: '5',
+        reviews: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! '
+        ]
+    },
+
+    {
+        title: 'Краткая история Земли. Четыре миллиарда лет в восьми главах',
+        annotation: 'Хорошо ли мы знаем землю под нашими ногами Скорее всего, то место, где сейчас стоит ваш дом, когда-то омывалось бурлящими потоками лавы, пряталось под толстым ледяным покровом, сотрясалось от удара метеорита, задыхалось от ядовитых газов, утопало в океане, возносилось на вершину горного хребта или давало приют устрашающим доисторическим животным.',
+        author: 'Нолл Эндрю',
+        category: 'Нехудожественная литература',
+        genre: 'Естественные науки',
+        cost: '625 ₽',
+        stars: '5',
+        reviews: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! '
+        ]
+    },
+
+    {
+        title: 'Краткая история Земли. Четыре миллиарда лет в восьми главах',
+        annotation: 'Хорошо ли мы знаем землю под нашими ногами Скорее всего, то место, где сейчас стоит ваш дом, когда-то омывалось бурлящими потоками лавы, пряталось под толстым ледяным покровом, сотрясалось от удара метеорита, задыхалось от ядовитых газов, утопало в океане, возносилось на вершину горного хребта или давало приют устрашающим доисторическим животным.',
+        author: 'Нолл Эндрю',
+        category: 'Нехудожественная литература',
+        genre: 'Естественные науки',
+        cost: '625 ₽',
+        stars: '5',
+        reviews: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Temporibus soluta sunt officia alias ipsa eos ad accusamus! Aspernatur dolores facere accusantium assumenda et nostrum similique reprehenderit, eaque nulla. Atque, consequatur earum. Omnis neque, magnam praesentium molestias dicta numquam harum. Est magni soluta, voluptates facere illo iure neque debitis nam dolorum harum fugiat suscipit quidem consequatur cum perferendis sit. Dolor in minus mollitia pariatur praesentium atque quod.',
+
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptas fuga sapiente maxime obcaecati voluptate! Recusandae atque consequatur sint, ipsum architecto rerum sequi hic nam veritatis ratione et molestias distinctio porro placeat maiores dolore commodi quo illum, fugiat deleniti ex necessitatibus iure! '
+        ]
+    },
+]
